505a65a7049d28d3cdda2cca5b4246cf7a80be82
MujahedTwair <mojad.tweir@gmail.com>
2023-10-30 15:45:57 +0200

    update ..

--- src/modules/category/category.router.js
+++ src/modules/category/category.router.js
@@ -4,6 +4,9 @@ import fileUpload, { fileValidation } from "../../services/multer.js";
 const router = Router();
 
 router.get('/', categoryController.getCategories);
+router.get('/active', categoryController.getActiveCategoies);
+router.get('/:id',categoryController.getSpecificCategory);
 router.post('/', fileUpload(fileValidation.image).single('image'), categoryController.createCategory);
+router.put('/:id', fileUpload(fileValidation.image).single('image'), categoryController.updateCategory);
 
 export default router;
